b0dded202382428fd636e6dab7dd8a38efb9ae2c
Fernando <atr.frano@gmail.com>
2017-04-03 22:28:51 -0300

    Fix $injector

--- auth/configurations/shared.config.js
+++ auth/configurations/shared.config.js
@@ -6,15 +6,15 @@
     'use strict';
     angular.module('auth.jwt').config(sharedConfig);
 
-    sharedConfig.$inject = ['$inject', '$httpProvider', 'jwtOptionsProvider', 'authJwtProvider'];
+    sharedConfig.$inject = ['$injector', '$httpProvider', 'jwtOptionsProvider', 'authJwtProvider'];
 
-    function sharedConfig($inject, $httpProvider, jwtOptionsProvider, authJwtProvider) {
+    function sharedConfig($injector, $httpProvider, jwtOptionsProvider, authJwtProvider) {
         var config = {
             tokenGetter: authJwtProvider.tokenGetter,
             unauthenticatedRedirector: authJwtProvider.unauthenticatedRedirector
         };
 
-        var whiteList = $inject.get('auth.jwt.whiteList');
+        var whiteList = $injector.get('auth.jwt.whiteList');
         if (whiteList) config.whiteListedDomains = whiteList;
 
         jwtOptionsProvider.config(config);
